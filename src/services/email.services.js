b2e5258a2c5b04cd0b652ece88a6ef13ac9ca60b
Timmy Code <oyewoleoluwatimilehin2@gmail.com>
2026-09-23 04:50:12 -0700

    update

--- src/services/email.services.js
+++ src/services/email.services.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST || "smtp.example.com",
+  host: process.env.SMTP_HOST || "smtp.gmail.com",
   port: process.env.SMTP_PORT || 587,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
@@ -19,12 +19,14 @@ export const sendOTP = async (email, otp, context = "Your OTP") => {
     const info = await transporter.sendMail({
       from: `"Support" <${process.env.SMTP_USER}>`,
       to: email,
-      subject: `${context}Code`,
-      text: ` Your OTP is ${otp}. It will expire in 5mins  `,
-      html: `<p>Your OTP code is <b>${otp}</b> </p>  `,
+      subject: `${context} Code`,
+      text: `Your OTP is ${otp}. It will expire in 15 minutes.`,
+      html: `<p>Your OTP code is <b>${otp}</b>.</p><p>It will expire in 5 minutes.</p>`,
     });
+    console.log("Message sent: %s", info.messageId);
+    return info;
   } catch (error) {
-    console.error("Error sendin email");
+    console.error("Error sending email:", error);
     throw error;
   }
 };
